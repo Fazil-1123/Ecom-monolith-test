b61ecc1493608fdeedf3a90f82b979b4186693c0
Fazil-1123 <152527893+Fazil-1123@users.noreply.github.com>
2025-08-13 12:43:47 +0200

    refactor(userTest): refactor code

--- src/test/java/com/ecom/monolith/controller/UserControllerTest.java
+++ src/test/java/com/ecom/monolith/controller/UserControllerTest.java
@@ -20,7 +20,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
-@Import(GlobalExceptionHandler.class)
 public class UserControllerTest {
 
     @Autowired
